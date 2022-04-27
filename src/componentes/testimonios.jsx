ad30a60a1d91ace8ce19baf25e7f62c6f4cef379
Mr.Blue <miguelrodriguezcastle@gmail.com>
2022-04-27 13:04:39 -0300

    two

--- src/componentes/testimonios.jsx
+++ src/componentes/testimonios.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+import '../stylesheets/Testimonios.css'
 
-function Testimonio() {
+
+function Testimonio(props) {
+
   return(
+
     <div className='contenedor-testimonio'>
+
       <img 
         className='imagen-testimonio'
-        src='../imagenes/Emma.png'
-      />
+        src={require(`../imagenes/${props.imagen}.png`)}
+        alt='Foto' />
+
+      <div className='contenedor-texto-testimonio'>
+
+        <p className='nombre'>
+          <strong>{props.nombre}</strong> en {props.pais}</p>
+
+        <p className='cargo'>
+          {props.cargo} en <strong>{props.empresa}</strong></p>
+
+        <p className='texto'>
+          "{props.testimonio}"</p>
+
+      </div>
+
+
     </div>
   );
 }
+
+export default Testimonio;
